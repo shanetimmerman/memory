42152092fbdeedef4c4a0cf092cfa0b146578213
shanetimmerman <shanetimmerman@gmail.com>
2018-09-25 16:18:19 -0400

    moved to one component

--- assets/js/memory.jsx
+++ assets/js/memory.jsx
@@ -23,32 +23,124 @@ class Memory extends React.Component {
         let list_of_tiles = [...Array(8).keys()];
         let list_of_pairs = list_of_tiles.concat(list_of_tiles);
         let tiles = this.randomizeTiles(list_of_pairs);
+        let visibility_pairs = tiles.map((x) =>
+         {return {value: x, visible: false}})
 
         this.state = {
-            tiles: tiles,
+            // Represents the board state
+            board: visibility_pairs,
+
+            // Number of tiles clicked
+            score: 0,
+            // Temporary store for index of the first clicked tile
+            // in the data array.
+            firstClicked: null,
         };
 
-        this.board = React.createRef();
+        this.incrementScore = this.incrementScore.bind(this);
     }
 
     /**
-     * Shuffles and hide all tiles.
+     * Shuffles and hide all tiles, sets score back to 0.
      */
     restart() {
-        this.setState({tiles: this.randomizeTiles(this.state.tiles)});
-        this.board.current.reset();
+        let shuffled_tiles = this.randomizeTiles(this.state.board);
+        let reset_tiles = shuffled_tiles.map((x) =>
+            {return { value: x.value, visible: false }});
+
+        this.setState({ board: reset_tiles,
+                        score: 0, first,
+                        firstClicked: null });
     }
 
     /**
-     * Randomizes the ordering of a given array
-     * @param arr: array to shuffle
-     * @returns {*}: Shuffled array
+     * randomizeTiles - Randomizes the ordering of an array
+     *
+     * @param  {Array} arr array to shuffle
+     * @return {Array}     Shuffled array
      */
     randomizeTiles(arr) {
         return arr.sort(function (t1, t2) {
             return 0.5 - Math.random()
         });
     }
+
+     /**
+      * incrementScore - Incremenents the score
+      *
+      * @return {type}  Void
+      */
+     incrementScore() {
+       this.setState({ score: this.state.score + 1 });
+     }
+
+     /**
+      * checkMatch - Handles clicks and checking for matches
+      *
+      * Called twice per potential mathing:
+      *   - On first call, shows the clicked tile and stores its index
+      *   - On second call, shows the clicked tile and checks its value vs
+      *       the previously clicked tile's. If they match, check if the game
+      *       has been won. If the don't, wait a bit so the user can see the
+      *       tile values then hide both tiles.
+      *
+      * @param  {type} index index of the clicked tile in this.state.board
+      * @return {type}       Void
+      */
+     checkMatch(index) {
+         // Bubbles up the click to the Game
+         this.incrementScore();
+
+         let board_copy = this.state.board;
+         let this_click = board_copy[index];
+         this_click.visible = true;
+
+         if (this.state.firstClicked == null) {
+             // If this is the first click, save the index and show the tile
+             this.setState({ board: board_copy, firstClicked: index, });
+         } else {
+             // If this is the second click, show the tile
+             this.setState({ board: board_copy });
+
+             let first_click = this.state.firstClicked;
+
+             // Check if both tiles have matching values
+             if (first_click.value == this_click.value) {
+                this.checkWin();
+
+             } else {
+
+                 // If the tiles do not match, hide them after 1 seconds,
+                 // and remove the stored first click value.
+                 let hide_tiles = () => {
+                     board_copy[index] = false;
+                     board_copy[first_click] = false;
+                     this.setState({ board: board_copy })
+                 };
+                 setTimeout(hide_tiles, 1000);
+             }
+             this.setState({ firstClicked: null });
+         }
+     }
+
+
+     /**
+      * checkWin - Checks if all tiles are visible (i.e. all pairs have
+      *            been matched).
+      *
+      * If the game is one, it will display an alert.
+      *
+      * @return {type}  Void
+      */
+     checkWin() {
+         // If they do, check if all tiles are visable
+         if (this.state.board.every((x) => x.revealed)) {
+             // If they are, alert the uer the won
+             // Delayed as board does not update as fast as the alert
+             // would go off. This allows the user to see all tiles.
+             setTimeout(() => alert("You WIN!"), 100);
+         }
+     }
 
     /**
      * Renders the memory game
@@ -61,129 +153,62 @@ class Memory extends React.Component {
         return <div className="column">
 
             <h1>Memory Game!</h1>
-            <Board tiles={this.state.tiles} ref={this.board} key="gameBoard"/>
-            <button onClick={() => this.restart()}>Restart Game</button>
+            <Board
+                board={ this.state.board }
+                checkMatch={ this.checkMatch.bind(this) }
+                key="gameBoard"/>
+            <div className="row">
+                <p>Score: { this.state.score }</p>
+                <button onClick={() => this.restart()}>Restart Game</button>
+            </div>
         </div>
     }
 }
 
-/**
- Represents the game board.
 
- Displays the tiles, keeps track of whats been clicked,
- and whats been matched.
- */
-class Board extends React.Component {
-    constructor(props) {
-        super(props);
+ /**
+  * class Board - Renders the game board (grid of tiles)
+  *
+  * @param  {type} props Properties of the board
+  * @return {type}       HTML to render for a <Board/>
+  */
+function Board(props) {
+    console.log(props);
+    // Generates the HTML for a 4 x 4 game board.
+    let board = [];
+    for (var i = 0; i < 4; i++) {
+        let row = [];
 
-        this.state = {
-            // Create an array mirroring props.tiles
-            // representing the visibility of each tile.
-            revealed: props.tiles.map((x) => false),
-
-            // Temporary store for index of the first clicked tile
-            // in the data array.
-            firstClicked: null,
-        };
-
-        this.checkMatch = this.checkMatch.bind(this);
-    }
-
-    /**
-     * Resest the state so all tiles are hidden.
-     */
-    reset() {
-        this.setState({revealed: this.state.revealed.map((x) => false), firstClicked: null});
-    }
-
-    /**
-     * Handles checking for matches, called twice per potential mathing:
-     *
-     * On first call, shows the clicked tile and stores its index
-     * On second call, shows the clicked tile and checks its value vs
-     * the previously clicked tile's. If they match, check if the game has
-     * been won. If the don't, wait a bit so the user can see the tile values,
-     * then hide both tiles.
-     *
-     * @param index: index of the clicked tile in this.props.tiles
-     */
-    checkMatch(index) {
-        let clicked = this.props.tiles[index];
-        let revealed = this.state.revealed;
-        revealed[index] = true;
-
-        if (this.state.firstClicked == null) {
-            // If this is the first click, save the index and show the tile
-            this.setState({revealed: revealed, firstClicked: index,});
-        } else {
-            // If this is the second click, show the tile
-            this.setState({revealed: revealed});
-            let first_index = this.state.firstClicked;
-            let last_clicked = this.props.tiles[first_index];
-
-            // Check if both tiles have matching values
-            if (last_clicked == clicked) {
-
-                // If they do, check if all tiles are visable
-                if (this.state.revealed.every((x) => x)) {
-
-                    // If they are, alert the uer the won
-                    // Delayed as board does not update as fast as the alert
-                    // would go off. This allows the user to see all tiles.
-                    setTimeout(() => alert("You WIN!"), 100);
-                }
-            } else {
-
-                // If the tiles do not match, hide them after 0.5 seconds,
-                // and remove the stored first click value.
-                let hide_tiles = () => {
-                    revealed[index] = false;
-                    revealed[first_index] = false;
-                    this.setState({revealed: revealed})
-                };
-                setTimeout(hide_tiles, 500);
-            }
-            this.setState({firstClicked: null});
-        }
-    }
-
-    /**
-     * Renders the game board (4 x 4)
-     * @returns {*}
-     */
-    render() {
-
-        // Generates the HTML for a 4 x 4 game board.
-        let board = [];
-        for (var i = 0; i < 4; i++) {
-            let row = [];
-
-            for (var k = 0; k < 4; k++) {
-                let index = i * 4 + k;
-                row.push(<Tile value={this.props.tiles[index]}
-                               revealed={this.state.revealed[index]}
-                               handleClick={(x) => this.checkMatch(x)}
-                               index={index} key={index}/>);
-            }
-
-            board.push(<div className="row"> {row}</div>);
+        for (var k = 0; k < 4; k++) {
+            let index = i * 4 + k;
+            row.push(<Tile value={ props.board[index].value }
+                           revealed={ props.board[index].visible }
+                           handleClick={ (x) => props.checkMatch(x) }
+                           index={ index }
+                           key={ index }/>);
         }
 
-        return <div className="column"> {board} </div>
+        board.push(<div className="row"> {row}</div>);
     }
+
+    return <div className="column"> {board} </div>
 }
 
-/**
- * Renders a tile element
- * @param props: Properties of the tile
- * @returns {*}
- * @constructor
- */
+
+ /**
+  * Tile - Renders a tile element
+  *
+  * @param  {type} props Properties of the tile
+  * @return {type}       HTML to render for a <Tile />
+  */
 function Tile(props) {
+    // Converts the internal value to the corresponinding capital letter.
+    // Only works if the number of pairs is <= 26.
+    let display = String.fromCharCode(97 + props.value);
+
     // If the tile has been revealed, show its value and disable clicking on it
     if (props.revealed) {
-        return <button>{props.value}</button>;
+        return <button>{display}</button>;
     } else {
         // Otherwise, hide the value (show a '?') and propogate clicks up to the
         // handleClick function of the board
